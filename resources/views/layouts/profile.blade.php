b1e3ee9e783eec23f06fcf0ee8e3227230c2c0e0
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-11 20:42:41 +0800

    Update brand logo

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -71,7 +71,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="{{asset('adminlte/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div -->
         <div class="info" style="margin: auto;">
-          {{ Auth::user()->name }}
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
