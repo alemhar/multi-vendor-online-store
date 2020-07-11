a85ce435d6938ea1cd722b6911f96116667a1a85
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-11 20:22:03 +0800

    Update brand logo

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -57,8 +57,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">osk_logo_full
-      <img src="{{asset('adminlte/img/osk_logo_full.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="index3.html" class="brand-link">
+      <img src="{{asset('adminlte/img/osk_logo_full.png')}}" alt="Online Store Ko Logo" class="brand-image elevation-3"
            style="opacity: .8">
       <!-- span class="brand-text font-weight-light"><span>assistant</span><span style="color: #F9BF3B;"><b>mo</b></span></span -->
     </a>
