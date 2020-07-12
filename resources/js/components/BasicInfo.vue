0547c4f259f403170f0c9769c1da3fd684dc7dd8
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-12 11:52:13 +0800

    Update BasicInfo Vue

--- resources/js/components/BasicInfo.vue
+++ resources/js/components/BasicInfo.vue
@@ -114,7 +114,6 @@
                             </div>
                             </form>
                         
-                        
 
                     </div>
                 </div>
@@ -122,6 +121,45 @@
         </div>
     </div>
 </template>
+<style>
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 140px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 150%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
 
 <script>
     export default {
@@ -149,16 +187,13 @@
         },
         methods: {
             copyToClipboard(myInput){
-                  /* Get the text field */
+                /* Get the text field */
                 let copyText = document.getElementById(myInput);
-
                 /* Select the text field */
                 copyText.select();
                 copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
                 /* Copy the text inside the text field */
                 document.execCommand("copy");
-
                 /* Alert the copied text */
                 alert("Copied the text: " + copyText.value);
             }
