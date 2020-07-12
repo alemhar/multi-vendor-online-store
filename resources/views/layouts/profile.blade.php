47f20c3b5c87907c244a88d44117b8da60f9dadb
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-12 09:51:32 +0800

    Added logout button to profile.blade

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -141,7 +141,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </li>
             </ul>
           </li>
-          
+          <li class="nav-item">
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
