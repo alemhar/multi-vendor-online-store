c5545bb58db7bb141c35a5cb749a6f94af98aefd
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-12 10:07:45 +0800

    Added logout button to profile.blade

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -93,10 +93,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
           
           <li class="nav-item">
             <router-link to="/products" class="nav-link">
-              <i class="nav-icon fas fa-user-cog"></i>
+              <!-- i class="nav-icon fas fa-user-cog"></i -->
+              <i class="nav-icon fas fa-boxes"></i>
               <p>
                 Products
-               
               </p>
             </router-link>
           </li>
