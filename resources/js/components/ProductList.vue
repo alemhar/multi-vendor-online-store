424221a7f6e489974e61c6a703223122fc2151e0
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-19 14:18:02 +0800

    Create Product List Page

--- resources/js/components/ProductList.vue
+++ resources/js/components/ProductList.vue
@@ -33,7 +33,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr v-for="product in products" :key="product.id">
+                                    <tr v-for="product in products.data" :key="product.id">
                                         <td>{{product.id}}</td>
                                         
                                         <td>{{product.name}}</td>
@@ -192,7 +192,7 @@
                 axios.get("api/product/")
                 .then((data)=>{
 
-                    this.products = data.data;
+                    this.products = data;
                     //this.public_address = window.location.hostname + '/main/' + this.form.public_id;
                   
                   //console.log( this.form.user_logo ? this.form.user_logo : 'img/your_logo_here.png');
