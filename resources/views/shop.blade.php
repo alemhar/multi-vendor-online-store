b1c9da196d1c4a6f42ec008fb87fa488e90d0250
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-22 22:55:17 +0800

    Added Photo for Add New Product

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -20,7 +20,7 @@
                         <div class="form">
                         @foreach ($products as $product)
                     
-                            <div class=" row">
+                            <div class="row mb-3">
                                 <div class="col-4">
                                     <div class="card">
                                         @if ($product->product_photo)
