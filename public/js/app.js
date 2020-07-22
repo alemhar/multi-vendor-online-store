704638623d4b0f901b9d6345144df86526d1f431
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-22 19:21:42 +0800

    Added Photo for Add New Product

--- public/js/app.js
+++ public/js/app.js
@@ -2677,6 +2677,7 @@ __webpack_require__.r(__webpack_exports__);
         _this4.id = response.data.id; //this.current_product_photo = 'img/products/'+this.product_photo;
 
         _this4.current_product_photo = _this4.product_photo.length > 0 ? 'img/products/' + _this4.product_photo : 'img/products/226x180.svg';
+        console.log(_this4.current_product_photo);
       })["catch"](function () {});
       this.showProductForm = true;
       this.productEditMode = true;
