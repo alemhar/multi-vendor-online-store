450dcce4775f476510a96659aea79b31368a97c0
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-22 22:32:04 +0800

    Added Photo for Add New Product

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -18,60 +18,27 @@
                     </div>
                     <div class="card-body">
                         <div class="form row">
-                            
-                            <div class="card-deck mb-3">
+                        @foreach ($products as $product)
+                    
+                            <div class="col">
                                 <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <img class="card-img-top" src="{{ $product->product_photo }}" alt="Card image cap">
                                     <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title">{{ $product->product_name }}</h5>
+                                    <p class="card-text">{{ $product->product_description }}</p>
                                     </div>
                                     <div class="card-footer">
-                                    <!-- a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-images"></i></a -->
+                                    
                                     <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
                                     <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cash-register"></i></a>
                                     <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
                                     </div>
                                 </div>
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i style="font-family:sans-serif;font-size: .6em;font-weight: 700;">BUY</i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i style="font-family:sans-serif;font-size: .6em;font-weight: 700;">BUY</i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i style="font-family:sans-serif;font-size: .6em;font-weight: 700;">BUY</i></a>
-                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
-                                    </div>
-                                </div>
+                            
                             </div>
 
-                            
+                            <p>Item: {{ $product->product_name }}</p>
+                        @endforeach    
 
 
                             <div class="form-row profile-photo-container">
@@ -89,7 +56,7 @@
                                 
                                 </div>
                             </div>
-                            </div> <!-- form -->
+                        </div> <!-- form -->
                         
 
                     </div>
