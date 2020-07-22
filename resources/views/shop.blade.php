b3e318034dbb5a1fb29b3b6362df580d974a699f
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-22 22:16:08 +0800

    Added Photo for Add New Product

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,89 +1,121 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>Online Store Ko</title>
+@section('content')
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h3>Product Page</h3> 
+                            </div>
+                            <div class="col-6">
+                                <button type="submit" @click="saveInfo" class="btn btn-primary float-right"><i class="fas fa-shopping-cart"></i></button>
+                                <button type="submit" @click="loadInfo" style="margin-right: 10px;" class="btn btn-outline-primary float-right">Contact Seller</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="form row">
+                            
+                            
+                            
+                            <div class="card-deck mb-3">
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <!-- a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-images"></i></a -->
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cash-register"></i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i style="font-family:sans-serif;font-size: .6em;font-weight: 700;">BUY</i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i style="font-family:sans-serif;font-size: .6em;font-weight: 700;">BUY</i></a>
+                                    <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+                            <div class="card-deck mb-3">
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    </div>
+                                    <div class="card-footer">
+                                    <small class="text-muted">Last updated 3 mins ago</small>
+                                    </div>
+                                </div>
+                            </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+                            <div class="form-row profile-photo-container">
+                                <input type="file" @change="profilePhotoChange" ref="file" style="display: none">
+                                <img class="profile-photo" id="profile-photo" :src="store_logo" alt="" style="height: 300px;">
+                                <div class="profile-middle">
+                                    <div class="profile-update-text" @click="$refs.file.click()">Update</div>
+                                
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <button type="submit" @click="saveInfo" class="btn btn-primary float-right">Save</button>
+                                    <button type="submit" @click="loadInfo" class="btn btn-default float-right">Cancel</button>
+                                
+                                </div>
+                            </div>
+                            </div> <!-- form -->
+                        
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            
-
-            <div class="content">
-                <div class="">
-                    SHOP
+                    </div>
                 </div>
-                @foreach ($products as $product)
-                    <p>Item: {{ $product->product_name }}</p>
-                @endforeach
-                <!-- div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div-->
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
