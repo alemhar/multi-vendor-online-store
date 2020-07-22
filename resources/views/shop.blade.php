6bd51a8569a2eab1d3a0fc8f27e6c7deddd1326f
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-22 22:27:19 +0800

    Added Photo for Add New Product

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -19,8 +19,6 @@
                     <div class="card-body">
                         <div class="form row">
                             
-                            
-                            
                             <div class="card-deck mb-3">
                                 <div class="card">
                                     <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
@@ -61,38 +59,7 @@
                                 </div>
                             </div>
 
-                            <div class="card-deck mb-3">
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <small class="text-muted">Last updated 3 mins ago</small>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <small class="text-muted">Last updated 3 mins ago</small>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <img class="card-img-top" src="img/products/226x180.svg" alt="Card image cap">
-                                    <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                    <small class="text-muted">Last updated 3 mins ago</small>
-                                    </div>
-                                </div>
-                            </div>
+                            
 
 
                             <div class="form-row profile-photo-container">
