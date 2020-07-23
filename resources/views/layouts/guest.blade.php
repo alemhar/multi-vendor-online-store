bcc5785f4b19aded89e46c36bea358b1ba8b564e
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-23 22:19:52 +0800

    Added Photo for Add New Product

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
         <meta name="user-id" content="{{ Auth::user()->id }}" />
     @endif
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- script src="{{ asset('js/app.js') }}" defer></script -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
