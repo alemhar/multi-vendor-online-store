832a887ded665db9e75eab59cdb2f9a0aa7754b4
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-24 23:49:46 +0800

    Added Public View | Products

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -3,7 +3,50 @@
     <div class="container">
         {{ public_id }}
         <div class="row justify-content-center">
-            
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h3>Product Page</h3> 
+                            </div>
+                            <div class="col-6">
+                                <!-- button type="submit" @click="saveInfo" class="btn btn-primary float-right"><i class="fas fa-shopping-cart"></i></button -->
+                                <!-- button type="submit" @click="loadInfo" style="margin-right: 10px;" class="btn btn-outline-primary float-right">Contact Seller</button -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="form">
+                        
+                    
+                            <div class="row mb-3" v-for="product in products.data" :key="product.id">
+                                <div class="col-md-4">
+                                    <div class="card">
+                                            <img class="card-img-top img-fluid" src="" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{product.name}}</h5>
+                                        </div>
+                                        <div class="card-footer">
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cash-register"></i></a>
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                        <p class="card-text">Product_description</p>
+                                    
+                                </div>
+                            </div>
+
+                            
+                        </div> <!-- form -->
+                        
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
