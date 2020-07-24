af2321454995d2f6b520b7a18e80bcef7daf7cb8
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-24 22:30:49 +0800

    Added Public View | Products

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -97,5 +97,16 @@
                 </div-->
             </div>
         </div>
+
+
+
+<!-- jQuery -->
+<script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('adminlte/js/adminlte.min.js')}}"></script>
+
+<script src="{{ asset('js/app.js')}}"></script>
     </body>
 </html>
