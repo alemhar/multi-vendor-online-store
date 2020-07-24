0ad682741c251ce89be02b7d853e84ceeb0fa2f6
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-24 21:44:44 +0800

    Added Public View | Products

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -30,22 +30,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                Online Store Ko
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <navbar></navbar>
+        <div class="container">
+            <products></products>
+        </div>
     </div>
 </body>
 </html>
