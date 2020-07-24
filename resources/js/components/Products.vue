5a89dc5531aa2ed18619d5835770bee17a66862d
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-25 00:34:35 +0800

    Added Public View | Products

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -6,7 +6,7 @@
         </div>
     </nav -->
     
-    <div class="container">
+    <div class="container mt-5">
   
         <div class="row justify-content-center">
             <div class="col-md-12">
