08972eee21ccbf9a207fe8bff9c7341980e18843
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-24 22:23:58 +0800

    Added Public View | Products

--- resources/js/app-guest.js
+++ resources/js/app-guest.js
@@ -7,8 +7,8 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-Vue.component('navbar', require('./components/NavBar.vue'));
-Vue.component('products', require('./components/Products.vue'));
+//Vue.component('navbar', require('./components/NavBar.vue'));
+//Vue.component('products', require('./components/Products.vue'));
 
 const app = new Vue({
   el: '#app'
