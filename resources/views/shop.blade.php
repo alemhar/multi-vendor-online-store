c580465eacda6ba634a811d81126d0049aa12910
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-24 21:51:01 +0800

    Added Public View | Products

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,83 +1,43 @@
-@extends('layouts.guest')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col-6">
-                                <h3>Product Page</h3> 
-                            </div>
-                            <div class="col-6">
-                                <button type="submit" @click="saveInfo" class="btn btn-primary float-right"><i class="fas fa-shopping-cart"></i></button>
-                                <!-- button type="submit" @click="loadInfo" style="margin-right: 10px;" class="btn btn-outline-primary float-right">Contact Seller</button -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="form">
-                        @foreach ($products as $product)
-                    
-                            <div class="row mb-3">
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        @if ($product->product_photo)
-                                        
-                                            <img class="card-img-top img-fluid" src="{{ asset('img/products/'. $product->product_photo)}}" alt="Card image cap">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                                            <!-- img src="{{ asset('path/to/asset.png') }}" -->
-                                        @else
-                                            <img  class="card-img-top img-fluid" src="{{ asset('img/products/226x180.svg') }}"  alt="Card image cap">
-                                        @endif
+    <title>Online Store Ko</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    @if (Auth::check())
+        <meta name="user-id" content="{{ Auth::user()->id }}" />
+    @endif
+    <!-- Scripts -->
+    <!-- script src="{{ asset('js/app.js') }}" defer></script -->
 
-                                        
-                                        
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{ $product->product_name }}</h5>
-                                            
-                                        </div>
-                                        <div class="card-footer">
-                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
-                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cash-register"></i></a>
-                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
-                                        </div>
-                                        
-                                    </div>
-                                    
-                                </div>
-                                <div class="col-md-8">
-                                        <p class="card-text">{{ $product->product_description }}</p>
-                                    
-                                </div>
-                            </div>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-                            
-                        @endforeach    
-
-
-                            <!-- div class="form-row profile-photo-container">
-                                <input type="file" @change="profilePhotoChange" ref="file" style="display: none">
-                                <img class="profile-photo" id="profile-photo" :src="store_logo" alt="" style="height: 300px;">
-                                <div class="profile-middle">
-                                    <div class="profile-update-text" @click="$refs.file.click()">Update</div>
-                                
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12">
-                                    <button type="submit" @click="saveInfo" class="btn btn-primary float-right">Save</button>
-                                    <button type="submit" @click="loadInfo" class="btn btn-default float-right">Cancel</button>
-                                
-                                </div>
-                            </div -->
-                        </div> <!-- form -->
-                        
-
-                    </div>
-                </div>
-            </div>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <navbar></navbar>
+        <div class="container">
+            <products></products>
         </div>
     </div>
-@endsection
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app-guest.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/shop2.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.guest')
+
+@section('content')
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <h3>Product Page</h3> 
+                            </div>
+                            <div class="col-6">
+                                <button type="submit" @click="saveInfo" class="btn btn-primary float-right"><i class="fas fa-shopping-cart"></i></button>
+                                <!-- button type="submit" @click="loadInfo" style="margin-right: 10px;" class="btn btn-outline-primary float-right">Contact Seller</button -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="form">
+                        @foreach ($products as $product)
+                    
+                            <div class="row mb-3">
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        @if ($product->product_photo)
+                                        
+                                            <img class="card-img-top img-fluid" src="{{ asset('img/products/'. $product->product_photo)}}" alt="Card image cap">
+
+                                            <!-- img src="{{ asset('path/to/asset.png') }}" -->
+                                        @else
+                                            <img  class="card-img-top img-fluid" src="{{ asset('img/products/226x180.svg') }}"  alt="Card image cap">
+                                        @endif
+
+                                        
+                                        
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $product->product_name }}</h5>
+                                            
+                                        </div>
+                                        <div class="card-footer">
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-eye"></i></a>
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cash-register"></i></a>
+                                            <a type="button" class="btn btn-outline-dark" href="#"><i class="fas fa-cart-plus"></i></a>
+                                        </div>
+                                        
+                                    </div>
+                                    
+                                </div>
+                                <div class="col-md-8">
+                                        <p class="card-text">{{ $product->product_description }}</p>
+                                    
+                                </div>
+                            </div>
+
+                            
+                        @endforeach    
+
+
+                            <!-- div class="form-row profile-photo-container">
+                                <input type="file" @change="profilePhotoChange" ref="file" style="display: none">
+                                <img class="profile-photo" id="profile-photo" :src="store_logo" alt="" style="height: 300px;">
+                                <div class="profile-middle">
+                                    <div class="profile-update-text" @click="$refs.file.click()">Update</div>
+                                
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <button type="submit" @click="saveInfo" class="btn btn-primary float-right">Save</button>
+                                    <button type="submit" @click="loadInfo" class="btn btn-default float-right">Cancel</button>
+                                
+                                </div>
+                            </div -->
+                        </div> <!-- form -->
+                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
