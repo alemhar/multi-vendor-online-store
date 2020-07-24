35cd7dc5b1ba6f5161ca2608c63aae375002df4e
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-25 00:02:31 +0800

    Added Public View | Products

--- public/js/app.js
+++ public/js/app.js
@@ -42019,7 +42019,7 @@ var render = function() {
                       _c("img", {
                         staticClass: "card-img-top img-fluid",
                         attrs: {
-                          src: "/img/products/" + _vm.product_photo,
+                          src: "/img/products/" + product.product_photo,
                           alt: "Card image cap"
                         }
                       }),
