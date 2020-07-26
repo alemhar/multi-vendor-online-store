a284a70117fb35345a8de6b9123793e42048abb2
Alemhar Salihuddin <alemhar@gmail.com>
2020-07-26 13:31:58 +0800

    Added Public View | Products

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -188,10 +188,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      "OSK" Ang online store mo.
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2010-2019 <a href="onlinestoreko.com">onlinestoreko.com</a>.</strong> support@onlinestoreko.com
   </footer>
 </div>
 <!-- ./wrapper -->
